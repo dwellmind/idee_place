db4eceba45d274107f88159aec2b99f077e25364
Pierre Le Gall <pierre@legall.im>
2022-01-04 15:54:55 +0100

    Handle query with ideas pagination

--- lib/idee_place_web/live/idea_index_live.ex
+++ lib/idee_place_web/live/idea_index_live.ex
@@ -3,7 +3,7 @@ defmodule IdeePlaceWeb.IdeaIndexLive do
 
   alias IdeePlace.Ideas
 
-  def mount(_params, %{"current_user" => current_user}, socket) do
+  def mount(_params, %{"current_user" => current_user, "query" => query}, socket) do
     ideas_page = Ideas.list_ideas(preload: :author, page_number: 1)
     starred_ideas_id = Ideas.list_starred_ideas_id_for(current_user)
 
@@ -13,18 +13,90 @@ defmodule IdeePlaceWeb.IdeaIndexLive do
         current_user: current_user,
         ideas_page: ideas_page,
         starred_ideas_id: starred_ideas_id,
-        query: ""
+        query: query
       ])
     }
   end
 
-  def handle_event("search", %{"search" => %{"query" => ""}}, socket) do
-    ideas = Ideas.list_ideas(preload: :author)
-
-    {:noreply, assign(socket, ideas: ideas)}
+  def mount(params, %{"current_user" => current_user}, socket) do
+    mount(params, %{"current_user" => current_user, "query" => ""}, socket)
   end
 
   def handle_event("search", %{"search" => %{"query" => query}}, socket) do
+    ideas_page = get_ideas_page(query, 1)
+    starred_ideas_id = Ideas.list_starred_ideas_id_for(socket.assigns.current_user)
+
+    {
+      :noreply,
+      assign(socket, [
+        ideas_page: ideas_page,
+        starred_ideas_id: starred_ideas_id,
+        query: query
+      ])
+    }
+  end
+
+  def handle_event("previous_page", %{"query" => query}, socket) do
+    previous_page_number = socket.assigns.ideas_page.page_number - 1
+
+    if previous_page_number < 1 do
+      raise "No more previous page, already on the first one."
+    end
+
+    ideas_page = get_ideas_page(query, previous_page_number)
+    starred_ideas_id = Ideas.list_starred_ideas_id_for(socket.assigns.current_user)
+
+    {
+      :noreply,
+      assign(socket, [
+        ideas_page: ideas_page,
+        starred_ideas_id: starred_ideas_id,
+        query: query
+      ])
+    }
+  end
+
+  def handle_event("next_page", %{"query" => query}, socket) do
+    next_page_number = socket.assigns.ideas_page.page_number + 1
+
+    if next_page_number > socket.assigns.ideas_page.total_pages do
+      raise "No more next page, last page is #{socket.assigns.ideas_page.total_pages}."
+    end
+
+    ideas_page = get_ideas_page(query, next_page_number)
+    starred_ideas_id = Ideas.list_starred_ideas_id_for(socket.assigns.current_user)
+
+    {
+      :noreply,
+      assign(socket, [
+        ideas_page: ideas_page,
+        starred_ideas_id: starred_ideas_id,
+        query: query
+      ])
+    }
+  end
+
+  def handle_event("go_to_page", %{"page" => page_number_string, "query" => query}, socket) do
+    page_number = String.to_integer(page_number_string)
+
+    if page_number < 1 or page_number > socket.assigns.ideas_page.total_pages do
+      raise "Page not in range (1..#{socket.assigns.ideas_page.total_pages})."
+    end
+
+    ideas_page = get_ideas_page(query, page_number)
+    starred_ideas_id = Ideas.list_starred_ideas_id_for(socket.assigns.current_user)
+
+    {
+      :noreply,
+      assign(socket, [
+        ideas_page: ideas_page,
+        starred_ideas_id: starred_ideas_id,
+        query: query
+      ])
+    }
+  end
+
+  defp get_ideas_page(query, page_number) do
     %{
       authors: authors,
       topics: topics,
@@ -32,74 +104,15 @@ defmodule IdeePlaceWeb.IdeaIndexLive do
       keywords: keywords
     } = Ideas.SearchParser.parse(query)
 
-    ideas =
-      Ideas.list_ideas(
-        preload: :author,
-        filters: [
-          authors: authors,
-          topics: topics,
-          user_stars: user_stars,
-          keywords: keywords
-        ]
-      )
-
-    {:noreply, assign(socket, ideas: ideas, query: query)}
-  end
-
-  def handle_event("previous_page", _, socket) do
-    previous_page_number = socket.assigns.ideas_page.page_number - 1
-
-    if previous_page_number < 1 do
-      raise "No more previous page, already on the first one."
-    end
-
-    ideas_page = Ideas.list_ideas(preload: :author, page_number: previous_page_number)
-    starred_ideas_id = Ideas.list_starred_ideas_id_for(socket.assigns.current_user)
-
-    {
-      :noreply,
-      assign(socket, [
-        ideas_page: ideas_page,
-        starred_ideas_id: starred_ideas_id
-      ])
-    }
-  end
-
-  def handle_event("next_page", _, socket) do
-    next_page_number = socket.assigns.ideas_page.page_number + 1
-
-    if next_page_number > socket.assigns.ideas_page.total_pages do
-      raise "No more next page, last page is #{socket.assigns.ideas_page.total_pages}."
-    end
-
-    ideas_page = Ideas.list_ideas(preload: :author, page_number: next_page_number)
-    starred_ideas_id = Ideas.list_starred_ideas_id_for(socket.assigns.current_user)
-
-    {
-      :noreply,
-      assign(socket, [
-        ideas_page: ideas_page,
-        starred_ideas_id: starred_ideas_id
-      ])
-    }
-  end
-
-  def handle_event("go_to_page", %{"page" => page_number_string}, socket) do
-    page_number = String.to_integer(page_number_string)
-
-    if page_number < 1 or page_number > socket.assigns.ideas_page.total_pages do
-      raise "Page not in range (1..#{socket.assigns.ideas_page.total_pages})."
-    end
-
-    ideas_page = Ideas.list_ideas(preload: :author, page_number: page_number)
-    starred_ideas_id = Ideas.list_starred_ideas_id_for(socket.assigns.current_user)
-
-    {
-      :noreply,
-      assign(socket, [
-        ideas_page: ideas_page,
-        starred_ideas_id: starred_ideas_id
-      ])
-    }
+    Ideas.list_ideas(
+      page_number: page_number,
+      preload: :author,
+      filters: [
+        authors: authors,
+        topics: topics,
+        user_stars: user_stars,
+        keywords: keywords
+      ]
+    )
   end
 end
